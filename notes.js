f9493b116d484b21e19694bdd793115df85b59e6
NewHellequin <tom.mightysloth.mills@gmail.com>
2023-10-02 22:11:48 +0100

    more stuff

--- notes.js
+++ notes.js
@@ -76,6 +76,35 @@ add(...x);
 restaurant.orderPizza('Mushrooms', 'Olives', 'Onions', 'Ham');
 restaurant.orderPizza('Mushrooms');
 
+console.log('----OR------');
+// Use ANY data type, return ANY data type, short-circuiting
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guest2 = restaurant.numGuests || 10;
+console.log(guest2);
+
+console.log('-----AND------');
+
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+
+console.log('Hello' && 23 && null && 'Jonas');
+
+// Practical example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('Mush', 'Spin');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('Mushroom', 'Spinach');
+
 // restaurant.orderDelivery({
 //   time: '22:30',
 //   address: 'Via del Sole, 21',
