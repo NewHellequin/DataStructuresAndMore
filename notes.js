26f4dbce26f0b2162ca1362a215b62282df5184a
NewHellequin <tom.mightysloth.mills@gmail.com>
2023-10-02 21:32:25 +0100

    REST ...

--- notes.js
+++ notes.js
@@ -36,7 +36,45 @@ const restaurant = {
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
+
+//SPREAD, because on the right side of =
+const arr = [1, 2, ...[3, 4]];
+
+//REST, because on the left side of the =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , Risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, Risotto, otherFood);
+
+// Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+// functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 4, 8, 3, 2, 8, 7, 5);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('Mushrooms', 'Olives', 'Onions', 'Ham');
+restaurant.orderPizza('Mushrooms');
 
 // restaurant.orderDelivery({
 //   time: '22:30',
