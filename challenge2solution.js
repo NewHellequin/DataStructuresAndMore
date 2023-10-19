f5453a583d31cca693e297fc8abd577902285e00
NewHellequin <tom.mightysloth.mills@gmail.com>
2023-10-19 21:46:50 +0100

    Challenge 2 Completed

--- challenge2solution.js
+++ challenge2solution.js
@@ -67,22 +67,17 @@ const game = {
 
 // 1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
 
-const a = [...game.scored];
-let c = [game.scored.length];
-let e = [game.scored];
-
-for (const b of e) {
-  for (let g = 0; g < game.scored.length; g++) {
-    console.log(`Goal ${g + 1}: `, game.scored[g]);
-  }
+for (const [i, player] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${player}`);
 }
 
 // 2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
 
-for (let i = 0; i < game.odds.length; i++) {
-  let avgOdds = i + i++;
-  console.log(avgOdds);
-}
+const odds = Object.values(game.odds);
+let average = 0;
+for (const odd of odds) average += odd;
+average /= odds.length;
+console.log(average.toFixed(1));
 
 // 3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
 //       Odd of victory Bayern Munich: 1.33
@@ -90,6 +85,11 @@ for (let i = 0; i < game.odds.length; i++) {
 //       Odd of victory Borrussia Dortmund: 6.5
 // Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names
 
-console.log(`Odd of vicotry ${game.team1}: ${game.odds.team1}`);
-console.log(`Odd of draw: ${game.odds.x}`);
-console.log(`Odd of vicotry ${game.team2}: ${game.odds.team2}`);
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr} ${odd}`);
+}
+
+// console.log(`Odd of vicotry ${game.team1}: ${game.odds.team1}`);
+// console.log(`Odd of draw: ${game.odds.x}`);
+// console.log(`Odd of vicotry ${game.team2}: ${game.odds.team2}`);
