c2a4913d6b92ae8133b40a8f27c0d1bc48a97cfb
NewHellequin <tom.mightysloth.mills@gmail.com>
2023-10-24 20:35:12 +0100

    Start of Challange 3

--- notes.js
+++ notes.js
@@ -42,38 +42,88 @@ const openingHours = {
   },
 };
 
-/////////////////////////////////
-// Maps
+///////////////////////////////////
+// Which data structures to use?
 
-const rest = new Map();
-rest.set('name', 'Classico Italiano');
-rest.set(1, 'Firenze, Italy');
-console.log(rest.set(2, 'Lisbon, Portugal'));
+// ////////////////////////////////
+// // Map Iteration
 
-rest
-  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-  .set('open', 1100)
-  .set('close', 2300)
-  .set(true, 'We are open')
-  .set(false, 'We are closed');
+// const question = new Map([
+//   ['question', 'What is the best programming language in the world?'],
+//   [1, 'C'],
+//   [2, 'Java'],
+//   [3, 'JavaScript'],
+//   ['correct', 3],
+//   [true, 'Correct'],
+//   [false, 'Try again!'],
+// ]);
 
-console.log(rest.get('name'));
-console.log(rest.get(true));
-console.log(rest.get(1));
+// console.log(question);
 
-const time = 2100;
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+// // Convert Object to map
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours));
+// console.log(hoursMap);
 
-console.log(rest.has('categories'));
-rest.delete(2);
+// // Quiz App
+// console.log(question.get('question'));
 
-const arr = [1, 2];
-rest.set(arr, 'Test');
-rest.set(document.querySelector('h1'), 'Heading');
-console.log(rest);
-console.log(rest.size);
+// for (const [key, value] of question) {
+//   if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+// }
+// let answer = Number(prompt('Your answer'));
+// answer = 3;
+// console.log(answer);
 
-console.log(rest.get(arr));
+// // work on the below...
+
+// // for (const [key, value] of question) {
+// //   if (answer === question.correct) console.log(`${true}`);
+// //   else {
+// //     console.log(`${false}`);
+// //   }
+// // }
+
+// // solution
+
+// console.log(question.get(question.get('correct') === answer));
+
+// // Convert Map to Array
+
+// console.log([...question]);
+
+// /////////////////////////////////
+// // Map Fundamentals
+
+// const rest = new Map();
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Firenze, Italy');
+// console.log(rest.set(2, 'Lisbon, Portugal'));
+
+// rest
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 1100)
+//   .set('close', 2300)
+//   .set(true, 'We are open')
+//   .set(false, 'We are closed');
+
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
+
+// const time = 2100;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+// console.log(rest.has('categories'));
+// rest.delete(2);
+
+// const arr = [1, 2];
+// rest.set(arr, 'Test');
+// rest.set(document.querySelector('h1'), 'Heading');
+// console.log(rest);
+// console.log(rest.size);
+
+// console.log(rest.get(arr));
 
 // //////////////////////////////////
 // // Sets
